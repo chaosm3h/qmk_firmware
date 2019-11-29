1a9648475519229b5c1061a8104ce8a8018197fa
chaosm3h <chaosm3h@gmail.com>
2019-11-29 13:54:49 +0900

    add keymaps of laplace

--- keyboards/keebio/laplace/keymaps/chaosm3h/keymap.c
+++ keyboards/keebio/laplace/keymaps/chaosm3h/keymap.c
@@ -17,20 +17,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     _______,   KC_BSPC,
     KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_COLN,  KC_QUOT,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_RSFT,  KC_ESC,
-    KC_LGUI,  KC_LALT,  FN1,      _______,  SP_FN1,   ET_FN2,                       FN2,      _______,  KC_EQL,   KC_SLSH
+    KC_LGUI,  KC_LALT,  FN1,      _______,  SP_FN1,   ET_FN2,                       FN2,      _______,  KC_EQL,   _______
   ),
 
   [_FN1] = LAYOUT(
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______,  KC_DEL,
     RESET,    RGB_HUI,  RGB_SAI,  RGB_VAI,  KC_VOLU,  _______,  _______,  _______,  _______,  KC_QUOT,  KC_SCLN,  _______,
-    RGB_MOD,  RGB_HUD,  RGB_SAD,  RGB_VAD,  KC_VOLD,  KC_LCBR,  KC_RCBR,  KC_1,     KC_LBRC,  KC_RBRC,  _______,  _______,
+    RGB_MOD,  RGB_HUD,  RGB_SAD,  RGB_VAD,  KC_VOLD,  KC_LCBR,  KC_RCBR,  _______,  KC_LBRC,  KC_RBRC,  _______,  _______,
     RGB_TOG,  _______,  _______,  _______,  _______,  _______,                      _______,  KC_MINS,  KC_PLUS,  _______
   ),
 
   [_FN2] = LAYOUT(
     KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  _______,  KC_DEL,
-    _______,  _______,  _______,  KC_INS,   KC_PGUP,  KC_HOME,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_COLN,  _______,
-    _______,  _______,  _______,  KC_DEL,   KC_PGDN,  KC_END,   KC_CIRC,  KC_DLR,   KC_UNDS,  _______,  _______,  _______,
+    _______,  _______,  _______,  KC_INS,   KC_PGUP,  KC_HOME,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_COLN,  KC_AT,
+    _______,  _______,  _______,  KC_DEL,   KC_PGDN,  KC_END,   KC_CIRC,  KC_DLR,   KC_UNDS,  KC_SLSH,  _______,  _______,
     _______,  _______,  _______,  _______,  KC_DEL,   _______,                      _______,  _______,  _______,  _______ 
   )
 };
